6e0423bea3e92222e421b863c36558ad39c1e187
HabeebuddinMir <habeebuddinmir@gmail.com>
2023-03-11 07:02:07 -0500

    password updated

--- shell1.sh
+++ shell1.sh
@@ -4,6 +4,6 @@ sudo yum upgrade -y
 sudo yum install -y mariadb-server
 sudo systemctl start mariadb
 mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('Coffeebites1$');"
-mysql -u root -p'root' -e "CREATE DATABASE IF NOT EXISTS user_management;"
+mysql -u root -p'Coffeebites1$' -e "CREATE DATABASE IF NOT EXISTS user_management;"
 sudo systemctl enable mariadb 
    
